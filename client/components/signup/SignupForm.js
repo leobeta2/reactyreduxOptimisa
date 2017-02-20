0d066c691550dc914bdf1247ed9cc95c71ee10ad
leonvidal <lvidal@optimisa.cl>
2017-02-20 17:50:49 -0300

    se incorporo el formulario de ingreso y los estados

--- client/components/signup/SignupForm.js
+++ client/components/signup/SignupForm.js
@@ -1,22 +1,38 @@
 import React from 'react'
+import timezones from '../../data/timezones'
+import map from 'lodash/map'
 
 class SignupForm extends React.Component {
      constructor(props)  {
          super(props);
          this.state = {
-             username: ''
+             username: '',
+             email: '',
+             password: '',
+             passwordConfirmation: '',
+             timezone: ''
          }
 
          this.onChange = this.onChange.bind(this);
+         this.onSubmit = this.onSubmit.bind(this);
      }
 
      onChange(e) {
+         //permite cambiar los estados, se coloco [e.target.name], para no nombrar por nombre
          this.setState({ [e.target.name]: e.target.value });
      }
 
+     onSubmit(e) {
+         e.preventDefault();
+         console.log(this.state);
+     }
+
     render() {
+        const options = map(timezones, (val, key) => 
+            <option key={val} value={val} >{key}</option>
+        ); 
         return (
-            <form>
+            <form onSubmit={this.onSubmit}>
                 <h1>Unete a nuestra comunidad!!</h1>
                 <div className="form-group">
                     <label className="control-label">Usuario</label>
@@ -26,6 +42,48 @@ class SignupForm extends React.Component {
                         type="text" 
                         name="username" 
                         className="form-control" />
+                </div>
+
+                <div className="form-group">
+                    <label className="control-label">Email</label>
+                    <input 
+                        value={this.state.email}
+                        onChange={this.onChange}
+                        type="text" 
+                        name="email" 
+                        className="form-control" />
+                </div>
+
+                <div className="form-group">
+                    <label className="control-label">Password</label>
+                    <input 
+                        value={this.state.password}
+                        onChange={this.onChange}
+                        type="password" 
+                        name="password" 
+                        className="form-control" />
+                </div>
+
+                <div className="form-group">
+                    <label className="control-label">Confirmar Password</label>
+                    <input 
+                        value={this.state.passwordConfirmation}
+                        onChange={this.onChange}
+                        type="password" 
+                        name="passwordConfirmation" 
+                        className="form-control" />
+                </div>
+
+                <div className="form-group">
+                    <label className="control-label">Zona horaria</label>
+                    <select
+                        value={this.state.timezone}
+                        onChange={this.onChange}
+                        name="timezone" 
+                        className="form-control" >
+                        <option value="" disabled>Escoge tu zona horaria</option>
+                        {options}
+                        </select>
                 </div>
 
                 <div className="form-group">
